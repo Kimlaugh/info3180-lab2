debbf23e57d73066c24fbe4bb47b8b6420326f02
Kimberly Laughton <kimberlyclaughton@gmail.com>
2023-02-12 10:53:40 -0500

    Added styling

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
+def format_date_joined(day, month, year):
+    _date = datetime.date(year, month, day)
+    format_date = "Joined " + _date.strftime("%B, %Y")
+    return format_date
 
 ###
 # Routing for your application.
@@ -16,6 +21,13 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', date=format_date_joined(21, 2, 2023))
+
+
+
 
 
 ###
@@ -45,3 +57,4 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
